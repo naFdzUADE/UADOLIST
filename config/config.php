693bb13946c81e6faca3f6fb5115f47b96c68005
nsh <77177369+nsh1z@users.noreply.github.com>
2025-04-14 17:27:29 -0300

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+// config/config.php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'uadolist');
+define('DB_USER', 'root');
+define('DB_PASS', ''); // Ajusta la contraseña según corresponda
+?>
